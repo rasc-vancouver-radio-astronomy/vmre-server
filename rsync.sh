0901df181871785886415daf4252aa4da893f397
Preston Thompson <prestonthompson@fastmail.com>
2022-04-19 18:11:16 -0700

    Minor html changes

--- rsync.sh
+++ rsync.sh
@@ -7,6 +7,6 @@ cp -rv events/* site/
 cp -rv plots/* site/
 cp -rv static/* site/
 cp -rv html/* site/
-ssh vmre@vmre.ca mkdir -p html/dev
-rsync -av --delete site/ vmre@vmre.ca:html/dev/
+ssh vmre@vmre.ca mkdir -p html/
+rsync -av --delete site/ vmre@vmre.ca:html/
 
