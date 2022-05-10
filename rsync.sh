5cfe49719fd4981e818370f08283d5fd538cce75
Preston Thompson <prestonthompson@fastmail.com>
2022-05-10 09:41:54 -0700

    Fixes for rsync.sh to handle large amounts of images

--- rsync.sh
+++ rsync.sh
@@ -2,11 +2,9 @@
 
 rm -rf site
 mkdir -p site
-cp -rv power/* site/
-cp -rv events/* site/
-cp -rv plots/* site/
-cp -rv static/* site/
-cp -rv html/* site/
+rsync -av plots/ site/
+rsync -av static/ site/
+rsync -av html/ site/
 ssh vmre@vmre.ca mkdir -p html/
 rsync -av --delete site/ vmre@vmre.ca:html/
 
